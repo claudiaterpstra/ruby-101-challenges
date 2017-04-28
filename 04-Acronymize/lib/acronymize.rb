c3726b2dd7ae5914007386fba1fdca7f47b1b450
Claudia Terpstra <claudia_terpstra@hotmail.com>
2017-04-28 17:05:50 +0100

    finish acronymize.rb

--- 04-Acronymize/lib/acronymize.rb
+++ 04-Acronymize/lib/acronymize.rb
@@ -1,4 +1,14 @@
 def acronymize(sentence)
   # TODO: return the acronym on the given sentence
-
+  array = sentence.downcase.scan(/\w+/)
+  new_array = []
+  array.each do |word|
+   new_array << word.chars.first
+  end
+  return new_array.join("").upcase
 end
+
+
+puts acronymize("As Soon As Possible")
+
+
