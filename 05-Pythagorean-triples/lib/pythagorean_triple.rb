b66642a311fe97c17d92d2ad3b12caf996066e52
Claudia Terpstra <claudia_terpstra@hotmail.com>
2017-06-27 17:06:42 +0200

    did exercise 5

--- 05-Pythagorean-triples/lib/pythagorean_triple.rb
+++ 05-Pythagorean-triples/lib/pythagorean_triple.rb
@@ -1,4 +1,4 @@
 def pythagorean_triple?(a, b, c)
   # TODO: return true/false depending if numbers a, b and c are Pythagorean triples
-
+    (a.is_a? Integer) && (b.is_a? Integer) && (c.is_a? Integer) && a^2 + b^2 == c^2
 end
