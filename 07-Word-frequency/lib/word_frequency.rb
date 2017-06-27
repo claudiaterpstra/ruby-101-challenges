d2305c1a859835a5b78087b7e13e8800e3f4dd76
Claudia Terpstra <claudia_terpstra@hotmail.com>
2017-06-27 17:55:03 +0200

    finished word-frequency

--- 07-Word-frequency/lib/word_frequency.rb
+++ 07-Word-frequency/lib/word_frequency.rb
@@ -1,4 +1,9 @@
 def word_frequency(sentence)
   # TODO: return a hash representing the number of apperances of words in the sentence
-
+  output = Hash.new(0)
+  splitted_sentence = sentence.downcase.gsub(/[^a-z0-9\s]/i, '').split(" ")
+  splitted_sentence.each do |word|
+    output[word] += 1
+  end
+  return output
 end
