38b9ea643fc39690ac6e52a58f5efaa39aa7af25
Claudia Terpstra <claudia_terpstra@hotmail.com>
2017-06-27 17:33:32 +0200

    created fizzbuzz

--- 06-FizzBuzz/lib/fizzbuzz.rb
+++ 06-FizzBuzz/lib/fizzbuzz.rb
@@ -1,4 +1,18 @@
 def fizzbuzz(max)
   # TODO: return an Array
-
+  array = []
+  (1..max).to_a.each do |n|
+    if (n%3 == 0) && (n%5 == 0)
+      array << "FizzBuzz"
+    elsif (n%3 == 0)
+      array << "Fizz"
+    elsif (n%5 == 0)
+      array << "Buzz"
+    else
+      array << n
+    end
+  end
+  return array
 end
+
+# puts fizzbuzz(15)
